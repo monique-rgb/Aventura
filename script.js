38b5d27ed4d53446576766b3279df5ab60d1820a
monique-rgb <vieira.silva.monique@escola.pr.gov.br>
2025-09-11 08:46:58 -0300

    ok

--- script.js
+++ script.js
@@ -5,8 +5,24 @@ avança.forEach(button => {
 button.addEventListener('click',function(){
     const atual = document.querySelector('.ativo');
     const proximoPasso='passo-'+this.getAttribute('data-proximo');
-})
+    atual.classList.remove("ativo");
+    cost proximoElemento= document.getElementById("proximoPasso");
 
+
+    cost avanca= document.querySelectorAll("btn.proximo");
+    cost reiniciarBtn= document.getElementById("btn.reiniciar");
+
+avanca.forEach{button =>{
+    button.addEventListener('click', function());
+    const atual= document.querySelector('.ativo');
+    const proximoPasso= 'passo-'+ this.getAttribute('data-proxima');
+    if (proximoElemento) {
+        proximoElemento.classList.add('ativo');
+    } else 
+}
+}
+})
+    
 
 }
 
